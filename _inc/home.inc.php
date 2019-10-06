2c620af6a72d94c942dbd25296f7c1c7533b7b4d
Gilberto Cortez <me@gilbertocortez.com>
2019-10-06 11:50:38 -0700

    Testing upload

--- _inc/home.inc.php
+++ _inc/home.inc.php
@@ -1,7 +1,7 @@
 <?php
 // Home Page
 ?>
-<h2>Social Media Management Software</h2>
+<h2>Social Media Management</h2>
 <h3>Handle all of your social media posting and management in one place!</h3>
 <p>Please log in via your Facebook account</p>
 <?php
