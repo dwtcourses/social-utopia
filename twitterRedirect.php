0865b6e3e09060ae684e4cff1ad018b8a4baea60
Gilberto Cortez <me@gilbertocortez.com>
2019-10-08 18:54:47 -0700

    modified twitter handler to include main loader file

--- twitterRedirect.php
+++ twitterRedirect.php
@@ -1,16 +1,9 @@
 <?php
-// Error Reporting On
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// Handler file for Twitter class
+// Handles initial log in auth and redirect
 
-// Start session in server
-session_start();
-
-// Load composer required files
-require_once "vendor/autoload.php";
-
-// Load Twitter Custom Class
-require_once ('_inc/class/twitterCustom.class.php');
+// Require loader file
+require_once('_inc/loader.inc.php');
 
 /* Build TwitterOAuth object with client credentials. */
 $_twitter = new twitterCustom();
