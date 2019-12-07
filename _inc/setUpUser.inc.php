75bc110e93eab4fbbf1179a513a0365966004d50
git@github.com <gilbertocortez@gilbertos-mbp.attlocal.net>
2019-12-07 12:04:39 -0800

    improved ux on sign up page

--- _inc/setUpUser.inc.php
+++ _inc/setUpUser.inc.php
@@ -2,8 +2,8 @@
 // Create user acount in database
 ?>
 <div class="container">
-	<h2><?= $_fb->getUserInformation()['name']; ?> Please Sign Up</h2>
-	<p>Hello! You need to create an account in order to use this app.</p>
-	<p>By clicking below you understand that your information will be stored, it will be used to better improve this app.</p>
-	<a href="signUp.php"><input type = "button" id = "signUp" class = "btn btn-primary"  value = "Sign Up"/></a>
+	<h2>Welcome <?= $_fb->getUserInformation()['name']; ?>!</h2>
+    <p>Hello! We noticed this is your first time visiting us, please sign up to get started. You need to create an custom environment with Social Utopia in order to take advantage of the features it has to offer.</p>
+	<p>By clicking the sign up button below, you understand that your information (name, email, connected account information, etc) will be stored on our serves, it will be used to better improve and test this application.</p>
+	<p style="text-align: center"><a href="signUp.php"><input type = "button" id = "signUp" class = "btn btn-primary btn-lg"  value = "Sign Up and Create Environment"/></a></p>
 </div>
