1d5e0807e709f2076f24448f6fc588b96b88ec12
git@github.com <me@gilbertocortez.com>
2019-12-23 14:11:46 -0800

    update nav links

--- _inc/view/nav.inc.php
+++ _inc/view/nav.inc.php
@@ -4,10 +4,10 @@
 						<a class="navbar-brand">Social Utopia</a>
 					</div>
 					<ul class="nav navbar-nav navbar-right">
-						<li class="active"><a href="/socialMediaApp">Home</a></li>
+						<li class="active"><a href="/">Home</a></li>
 						<li><a href="https://interactiveutopia.com">Interactive Utopia</a></li>
 						<li><a href="https://github.com/gilbertocortez/social-utopia">GitHub Repository</a></li>
-						<li><a href = "https://dev.interactiveutopia.com/socialMediaApp/logout.php">Log Out</a></li>
+						<li><a href = "/logout.php">Log Out</a></li>
 					</ul>
 				</div>
 			</nav>
