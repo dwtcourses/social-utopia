542b9da13b267d2083bb18bc8fad64e18b61400a
git@github.com <me@gilbertocortez.com>
2019-12-23 12:51:04 -0800

    modified home content

--- _inc/home.inc.php
+++ _inc/home.inc.php
@@ -4,8 +4,8 @@
 // User is not yet logged in
 ?>
 <div id="homeContent">
-    <h2 class="homeh2">Open-Source Social Media Management Environment</h2>
-    <h3>Handle all of your social media posting and management in one place!</h3>
+    <h2 class="homeh2">Social Media Management Done Right!</h2>
+    <h3>Handle all of your social media posting and management in one place! And what is the best part of it? That it is free!</h3>
     <div id="facebookLogInDiv">
         <i class="fab fa-facebook"></i>
         <p>To start please log in using Facebook</p>
