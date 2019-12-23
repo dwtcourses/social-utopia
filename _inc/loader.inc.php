6405cc95e43b0ada140a8e638851cf787a01195f
git@github.com <me@gilbertocortez.com>
2019-12-22 23:49:18 -0800

    Path repair on loader

--- _inc/loader.inc.php
+++ _inc/loader.inc.php
@@ -8,25 +8,26 @@
 
 if ( $_SERVER['DOCUMENT_ROOT'] == '/var/www/vhosts/interactiveutopia.com/subdomains/dev/httpdocs')
     $_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT'] . '/socialMediaApp/';
+else $_SERVER['DOCUMENT_ROOT'] = $_SERVER['DOCUMENT_ROOT'] . '/';
 
 // Load global application variables
-    require ( $_SERVER['DOCUMENT_ROOT'] . '/_inc/_private/auth.tokens.php');
+    require ( $_SERVER['DOCUMENT_ROOT'] . '_inc/_private/auth.tokens.php');
 
 // Load composer required files
-	require_once $_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php";
+	require_once $_SERVER['DOCUMENT_ROOT'] . "vendor/autoload.php";
 
 // Load MySQL Class
-    require_once($_SERVER['DOCUMENT_ROOT'] . '/_inc/class/mysql.class.php');
+    require_once($_SERVER['DOCUMENT_ROOT'] . '_inc/class/mysql.class.php');
     $_sql = new mysql();
 
 // Load Facebook Custom Class
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/_inc/class/facebookCustom.class.php');
+	require_once($_SERVER['DOCUMENT_ROOT'] . '_inc/class/facebookCustom.class.php');
 
 // Load Twitter Custom Class
-	require_once ($_SERVER['DOCUMENT_ROOT'] . '/_inc/class/twitterCustom.class.php');
+	require_once ($_SERVER['DOCUMENT_ROOT'] . '_inc/class/twitterCustom.class.php');
 
 // Load LinkedIn Custom Class
-	require_once ($_SERVER['DOCUMENT_ROOT'] . '/_inc/class/linkedIn.class.php');
+	require_once ($_SERVER['DOCUMENT_ROOT'] . '_inc/class/linkedIn.class.php');
 
 // Load Google API Custom Class
-	require_once ($_SERVER['DOCUMENT_ROOT'] . '/_inc/class/google.class.php');
+	require_once ($_SERVER['DOCUMENT_ROOT'] . '_inc/class/google.class.php');
